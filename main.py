dc6a48c8d73a5b6a30abcc9ce4823474259156f8
g0dswisdom <128635829+g0dswisdom@users.noreply.github.com>
2023-05-04 18:22:58 +0300

    Created command handler

--- main.py
+++ main.py
@@ -19,46 +19,45 @@ logo = pyfiglet.figlet_format("VBM")
 logo = f"{Fore.CYAN}{logo}"
 os_name = platform.release()
 
-def process_command(command):
-    if command == "neofetch":
-        system = platform.system()
-        cpu = cpuinfo.get_cpu_info()['brand_raw']
-        memory = psutil.virtual_memory()
-        disk = psutil.disk_usage('/')
+def neofetch(args):
+    system = platform.system()
+    cpu = cpuinfo.get_cpu_info()['brand_raw']
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
 
-        system_line = f"{Fore.CYAN}System:{Fore.WHITE} {system}\n"
-        version_line = f"{Fore.CYAN}Version:{Fore.WHITE} {os_name}\n"
-        cpu_line = f"{Fore.CYAN}CPU:{Fore.WHITE} {cpu}\n"
-        memory_line = f"{Fore.CYAN}Memory:{Fore.WHITE} {memory.percent}% used ({psutil._common.bytes2human(memory.used)} used / {psutil._common.bytes2human(memory.total)} total)\n"
-        disk_line = f"{Fore.CYAN}Disk:{Fore.WHITE} {disk.percent}% used ({psutil._common.bytes2human(disk.used)} used / {psutil._common.bytes2human(disk.total)} total)\n"
+    system_line = f"{Fore.CYAN}System:{Fore.WHITE} {system}\n"
+    version_line = f"{Fore.CYAN}Version:{Fore.WHITE} {os_name}\n"
+    cpu_line = f"{Fore.CYAN}CPU:{Fore.WHITE} {cpu}\n"
+    memory_line = f"{Fore.CYAN}Memory:{Fore.WHITE} {memory.percent}% used ({psutil._common.bytes2human(memory.used)} used / {psutil._common.bytes2human(memory.total)} total)\n"
+    disk_line = f"{Fore.CYAN}Disk:{Fore.WHITE} {disk.percent}% used ({psutil._common.bytes2human(disk.used)} used / {psutil._common.bytes2human(disk.total)} total)\n"
 
-        final_output = ""
-        final_output += f"{system_line}{version_line}{cpu_line}{memory_line}{disk_line}".center(os.get_terminal_size().columns)
-        final_output = f"{Fore.CYAN}{logo}\n{final_output}"
+    final_output = ""
+    final_output += f"{system_line}{version_line}{cpu_line}{memory_line}{disk_line}".center(os.get_terminal_size().columns)
+    final_output = f"{Fore.CYAN}{logo}\n{final_output}"
 
-        print(final_output)
-    elif command == "clear":
-        os.system('cls' if os.name == 'nt' else 'clear')
-    elif command.startswith("print "):
-        text = command[6:]
-        print(text)
-    elif command == "exit":
-        return False
-    elif command == "help":
-        print("Available commands:\n"
-              "neofetch - Show system information\n"
-              "clear - Clear the screen\n"
-              "exit - Exit the program\n"
-              "print textgoeshere - print the desired text\n"
-              "help - Show this help message")
+    print(final_output)
+
+def clear(args):
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def exit(args):
+    return False
+commands = {
+    "neofetch": neofetch,
+    "clear": clear,
+    "exit": exit,
+}
+
+def handleCommand(command):
+    parts = command.split(" ")
+    cmd = parts[0]
+    args = parts[1:]
+
+    if cmd in commands:
+        commands[cmd](args)
     else:
-        print(f"Command '{command}' not recognized.")
-    return True
-
-
+        print("That command doesnt exist")
 
 while True:
     command = input(f"{username}@{hostname}$ ")
-
-    if not process_command(command):
-        break
+    handleCommand(command)
